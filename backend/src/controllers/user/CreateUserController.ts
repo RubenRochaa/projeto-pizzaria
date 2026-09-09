383e4bc553d0cf7517550c036f9283d574899b48
Ruben Rocha <ruben993835627@gmail.com>
2026-09-08 21:02:43 -0300

    Back end do usuário finalizado

--- backend/src/controllers/user/CreateUserController.ts
+++ backend/src/controllers/user/CreateUserController.ts
@@ -1,10 +1,20 @@
 import { Request, response, Response } from "express";
+import { CreateUserService } from "../../services/user/CreateUserService";
+
 
 class CreateUserController {
     async handle(req: Request, res: Response) {
         const { name, email, password } = req.body;
 
-        res.json({ ok: true })
+        const createUserService = new CreateUserService();
+
+        const user = await createUserService.execute({
+            name,
+            email,
+            password
+        });
+
+        res.json(user)
     }
 }
 
--- backend/src/prisma/index.ts
+++ backend/src/prisma/index.ts
@@ -1,5 +1,6 @@
-import { PrismaClient } from '@prisma/client';
-
+import 'dotenv/config'; 
+// import adicionado para que o prisma consiga ler as variáveis de ambiente do arquivo .env
+import { PrismaClient } from '../generated/prisma';
 const prismaClient = new PrismaClient();
 
 export default prismaClient;
