8ca6c0ec28349c338a9335063d8a2f0cf1df6b62
Ruben Rocha <ruben993835627@gmail.com>
2026-09-11 00:52:04 -0300

    feat: adiciona estilos ao modal de pedidos e documenta o projeto

--- frontend/src/app/dashboard/components/orders/index.tsx
+++ frontend/src/app/dashboard/components/orders/index.tsx
@@ -1,7 +1,38 @@
-export function Orders(){
+import styles from './styles.module.scss'
+import { RefreshCw } from 'lucide-react'
+import { OrderProps } from '@/lib/order_type'
+import { Modalorder } from '../modal'
+
+interface Props{
+    orders: OrderProps[]
+}
+
+export function Orders({ orders }: Props){
     return(
-        <main>
-            <h1>PEDIDOS</h1>
+    <>
+        <main className={styles.container}>
+            
+            <section className={styles.containerHeader}>
+                <h1>Últimos pedidos</h1>
+                <button>
+                    <RefreshCw size={24} color='#3fffa3'/>
+                </button>
+            </section>
+
+            <section className={styles.listOrders}>
+                {orders.map( order => (
+                    <button 
+                    key={order.id}
+                    className={styles.orderItem}
+                    >
+                    <div className={styles.tag}></div>
+                    <span>Mesa {order.table}</span>
+                    </button>
+                ))}
+            </section>
         </main>
+
+        <Modalorder/>
+    </>
     )
 }
